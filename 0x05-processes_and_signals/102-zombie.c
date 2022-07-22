2c172f8c5671249f8de0dee68da184d9d90a7410
jhon <yohanneskelem@gmail.com>
2022-07-22 23:51:34 +0200

    tasks on bash, DevOps, shell, scripting

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int infinite_while(void);
+
+/**
+ * main - function that creates 5 zombie processes
+ * Return: always 0
+ */
+
+int main(void)
+{
+	pid_t child_pid;
+	int i = 0;
+
+	for (i = 0; i < 5; i++)
+	{
+		child_pid = fork();
+		if (child_pid)
+		{
+			printf("Zombie process created, PID: %d\n", child_pid);
+		}
+		else
+			exit(0);
+	}
+	infinite_while();
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * infinite_while - funtion that prevents the parent processe from returning
+ * Return: always 0
+ */
+
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
